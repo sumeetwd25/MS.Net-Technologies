bac50d38aea7130b8ec78c43d873a36dabefbcd5
Sumeet Wadile <sumeetwd25@gmail.com>
2023-07-13 09:31:32 +0530

    Add files via upload

---
+++ CTSDemoApp/Follower.cs
@@ -0,0 +1,13 @@
+namespace TeamBuilding{
+    public class Follower{
+        private string FirstName;
+
+        public Follower(string fName){
+            this.FirstName=fName;
+        }
+
+        public void setFistName(string name){
+            this.FirstName=name;
+        }
+    }
+}
